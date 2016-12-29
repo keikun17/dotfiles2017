636b5e8a83304f670dcffff8f0c1cc1fa668177b
Buddy Magsipoc <keikun17@gmail.com>
2016-12-29 14:30:19 +0800

    Update.

--- config/fish/config.d/ruby.fish
+++ config/fish/config.d/ruby.fish
@@ -1,7 +1,8 @@
 # https://github.com/JeanMertz/chruby-fish
 # load chruby wrapper for fish
+
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 
 # Default ruby
-chruby ruby-2.3.0
+chruby ruby-2.4.0
