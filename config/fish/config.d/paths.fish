3b1eba225108569fdd4a781e61d24cc53e6b8c1d
buddy magsipoc <keikun17@gmail.com>
2017-08-04 12:33:59 +0800

    Paths fix.

--- config/fish/config.d/paths.fish
+++ config/fish/config.d/paths.fish
@@ -1,4 +1,4 @@
 set -gx PATH "/home/linuxbrew/.linuxbrew/bin" $PATH
-set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH 
-set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH 
+set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH
+set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
 set -gx XDG_DATA_DIRS "/home/linuxbrew/.linuxbrew/share:$XDG_DATA_DIRS"
